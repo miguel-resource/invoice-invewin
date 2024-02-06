85a026100146fc6f80c14f03ef025639d753bcb9
miguel-resource <miguelbercru27@gmail.com>
2024-02-06 02:06:47 -0600

    [ADD] pdf grid

--- web/src/app/invoices/page.tsx
+++ web/src/app/invoices/page.tsx
@@ -2,10 +2,7 @@
 
 import TableGrid from "@/components/common/Table";
 import dynamic from "next/dynamic";
-import { Chip, Tooltip } from "@mui/material";
-import { useEffect, useLayoutEffect } from "react";
-import { getAllInvoices } from "@/services/Company";
-import { useSelector } from "react-redux";
+
 
 const MainWrapper = dynamic(() => import("@/components/MainWrapper/layaout"), {
   ssr: false,
@@ -14,105 +11,14 @@ export default function InvoicePage() {
 
 
 
-
   return (
     <MainWrapper>
       <div
-        className="panel panel-inverse h-full"
+        className="panel panel-inverse h-fit"
         data-sortable-id="form-stuff-1"
       >
         <div className="flex items-center justify-center h-full w-11/12 mx-auto mt-20 mb-20">
-          <TableGrid
-            columns={[
-              {
-                field: "date",
-                headerName: "Fecha",
-                type: "date",
-                width: 130,
-                editable: false,
-                headerAlign: "center",
-                align: "center",
-              },
-              {
-                field: "serie",
-                headerName: "Serie",
-                width: 120,
-                editable: false,
-                headerAlign: "center",
-                align: "center",
-              },
-              {
-                field: "folio",
-                headerName: "Folio",
-                width: 300,
-                editable: false,
-                headerAlign: "center",
-                align: "center",
-              },
-              {
-                field: "rfcClient",
-                headerName: "RFC del Cliente",
-                width: 200,
-                editable: false,
-                headerAlign: "center",
-                align: "center",
-              },
-              {
-                field: "nameClient",
-                headerName: "Razón Social",
-                width: 300,
-                editable: false,
-                headerAlign: "center",
-                cellAlign: "left",
-                align: "center",
-              },
-              {
-                field: "total",
-                headerName: "Total",
-                width: 130,
-                editable: false,
-                headerAlign: "center",
-                cellAlign: "center",
-                align: "center",
-              },
-              {
-                field: "methodPayment",
-                headerName: "Método de Pago",
-                width: 130,
-                editable: false,
-                headerAlign: "center",
-                renderCell: (params: any) => {
-                  return (
-                    <Chip
-                      label={params.value}
-                      className=" text-slate-100"
-                      variant="outlined"
-                      color="success"
-                    />
-                  );
-                },
-              },
-              {
-                field: "actions",
-                headerName: "",
-                width: 470,
-                headerAlign: "center",
-                renderCell: () => {
-                  return (
-                    <div className="flex justify-center space-x-2 mx-auto">
-                      <Tooltip title="Ver" arrow placement="right">
-                        <button className="p-1 w-11 rounded-full  ">
-                          <i className="fa fa-eye text-slate-700"></i>
-                        </button>
-                      </Tooltip>
-
-                     
-                    </div>
-                  );
-                },
-              },
-            ]}
-          />
+          <TableGrid/>
         </div>
       </div>
     </MainWrapper>
--- web/src/components/common/Table.tsx
+++ web/src/components/common/Table.tsx
@@ -1,30 +1,65 @@
 import { getAllInvoices } from "@/services/Company";
-import { Box } from "@mui/material";
+import { Chip, Tooltip } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useRouter } from "next/navigation";
 import { useEffect, useLayoutEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { faker } from "@faker-js/faker";
+import { jsPDF } from "jspdf";
 
-type Props = {
-  columns: any;
-};
 
-export default function TableGrid({ columns }: Props) {
+export default function TableGrid() {
   const router = useRouter();
   const company = useSelector((state: any) => state.company);
-  const companyLogin = useSelector((state: any) => state.loginCompany);  
+  const companyLogin = useSelector((state: any) => state.loginCompany);
 
-  // const [data, setData] = useState([]);
+  // const [data, setData] = useState([]); // TODO: Change to real data
 
+  const showDataPDF = (data: any) => {
+    const doc = new jsPDF();
+    doc.setProperties({
+      title: "Factura"
+    });
+
+    // Header 
+    doc.setFillColor(0, 0, 0);
+
+    
+  
+
+    doc.setFontSize(12);
+    doc.text("Your Company Name", 15, 10);
+    doc.text("Dirección", 15, 20);
+    doc.text("Ciudad, Estado", 15, 30);
+
+    const gridWidth = 180;
+    const gridHeight = 20;
+    const leftMargin = 10;
+    const topMargin = 70;
+    doc.setLineWidth(0.3);
+
+    doc.text("Fecha: ", leftMargin, topMargin + gridHeight);
+    doc.text(new Date(data.date).toLocaleDateString(), leftMargin + gridWidth / 13, topMargin + gridHeight);
+
+    doc.text("Serie: " + data.serie, leftMargin, topMargin + gridHeight * 2);
+    doc.text("Folio: " + data.folio, leftMargin + gridWidth / 2, topMargin + gridHeight * 2);
+    doc.text("RFC del Cliente: " + data.rfcClient, leftMargin, topMargin + gridHeight * 3);
+    doc.text("Razón Social: " + data.nameClient, leftMargin, topMargin + gridHeight * 4);
+
+
+    doc.line(leftMargin, topMargin + gridHeight * 7, leftMargin + gridWidth + 5, topMargin + gridHeight * 7); // Separator
+    doc.text("Método de Pago: " + data.methodPayment, leftMargin, topMargin + gridHeight * 8);
+    doc.text("Total: " + data.total, leftMargin, topMargin + gridHeight * 9);
+
+    doc.output("dataurlnewwindow");
+  };
   useLayoutEffect(() => {
     if (!company.rfc) {
       router.push("/login-supplier");
     }
   }, [company]);
 
-  const handleData = async () => {
-    console.log("COMPANY LOGIN", companyLogin);
+  const handleData = async () => { // TODO: Implement and discomment the useEffect
     const response = await getAllInvoices(
       companyLogin.email,
       companyLogin.password
@@ -65,18 +100,117 @@ export default function TableGrid({ columns }: Props) {
   }, []);
 
   return (
-      <DataGrid
-        rows={data}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
+    <DataGrid
+      rows={data}
+      columns={[
+        {
+          field: "date",
+          headerName: "Fecha",
+          type: "date",
+          width: 130,
+          editable: false,
+          headerAlign: "center",
+          align: "center",
+        },
+        {
+          field: "serie",
+          headerName: "Serie",
+          width: 130,
+          editable: false,
+          headerAlign: "center",
+          align: "center",
+        },
+        {
+          field: "folio",
+          headerName: "Folio",
+          width: 300,
+          editable: false,
+          headerAlign: "center",
+          align: "center",
+        },
+        {
+          field: "rfcClient",
+          headerName: "RFC del Cliente",
+          width: 200,
+          editable: false,
+          headerAlign: "center",
+          align: "center",
+        },
+        {
+          field: "nameClient",
+          headerName: "Razón Social",
+          width: 300,
+          editable: false,
+          headerAlign: "center",
+          align: "center",
+        },
+        {
+          field: "total",
+          headerName: "Total",
+          width: 130,
+          editable: false,
+          headerAlign: "center",
+          align: "center",
+        },
+        {
+          field: "methodPayment",
+          headerName: "Método de Pago",
+          width: 130,
+          editable: false,
+          headerAlign: "center",
+          renderCell: (params: any) => {
+            return (
+              <Chip
+                label={params.value}
+                className=" text-slate-100"
+                variant="outlined"
+                color="success"
+              />
+            );
           },
-        }}
-        pageSizeOptions={[5, 10, 20]}
-      />
-    
+        },
+        {
+          field: "actions",
+          headerName: "",
+          width: 470,
+          headerAlign: "center",
+          renderCell: (params: any) => {
+            return (
+              <div className="flex justify-center space-x-2 mx-auto">
+                <Tooltip title="Ver" arrow placement="right">
+                  <button
+                    onClick={() => showDataPDF(
+                      {
+                        date: params.row.date,
+                        serie: params.row.serie,
+                        folio: params.row.folio,
+                        rfcClient: params.row.rfcClient,
+                        nameClient: params.row.nameClient,
+                        total: params.row.total,
+                        methodPayment: params.row.methodPayment,
+                      }
+
+                    )}
+                    className="p-1 w-11 rounded-full  ">
+                    <i className="fa fa-eye text-slate-700"></i>
+                  </button>
+                </Tooltip>
+
+
+              </div>
+            );
+          },
+        },
+      ]}
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 20,
+          },
+        },
+      }}
+      pageSizeOptions={[10, 20, 50, 100]}
+    />
+
   );
 }

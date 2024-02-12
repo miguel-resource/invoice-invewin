3f713ac2c10fb2868d6adb230db8be22ea64c77e
miguel-resource <miguelbercru27@gmail.com>
2024-02-12 11:35:44 -0600

    [FIX] spinner stamp bill

--- web/src/components/Forms/EditClient.tsx
+++ web/src/components/Forms/EditClient.tsx
@@ -3,6 +3,7 @@ import CommonAlert from "@/components/common/Alert";
 
 import { getCatalogs } from "@/services/Catalog";
 import { useDispatch, useSelector } from "react-redux";
+import { CircularProgress } from "@mui/material";
 
 type Props = {
   onClose: () => void;
@@ -10,7 +11,11 @@ type Props = {
   isCreatingClient: boolean;
 };
 
-export default function EditClient({ onClose, formik, isCreatingClient }: Props) {
+export default function EditClient({
+  onClose,
+  formik,
+  isCreatingClient,
+}: Props) {
   const [message, setMessage] = useState("");
   const [type, setType] = useState<"success" | "error" | "warning" | "info">(
     "success"
@@ -19,9 +24,16 @@ export default function EditClient({ onClose, formik, isCreatingClient }: Props)
   const [catalogRegime, setCatalogRegime] = useState([]);
   const [catalogUseCFDI, setCatalogUseCFDI] = useState([]);
 
+  const areFieldsEmpty =
+    !formik.values.razonSocial ||
+    !formik.values.email ||
+    !formik.values.codigoPostal ||
+    !formik.values.regimenFiscal ||
+    !formik.values.usoCfdi;
+
   const handleGetCatalogs = async () => {
     const res = await getCatalogs();
-   
+
     setCatalogRegime(res.data.cRegimenFiscal.c_RegimenFiscal);
     setCatalogUseCFDI(res.data.cUsoCFDI.c_UsoCFDI);
   };
@@ -35,139 +47,137 @@ export default function EditClient({ onClose, formik, isCreatingClient }: Props)
     formik.setFieldValue("email", formik.values.email);
   }, [formik.values]);
 
-
   return (
-    <form
-      className="flex items-center justify-center h-full border-b-2 border-slate-200 mb-8"
-      onSubmit={formik.handleSubmit}
-    >
-      <div
-        className="  w-full
+    <>
+      {areFieldsEmpty ? (
+        <div className="flex justify-center items-center h-96">
+          <CircularProgress />
+        </div>
+      ) : (
+        <form
+          className="flex items-center justify-center h-full border-b-2 border-slate-200 mb-8"
+          onSubmit={formik.handleSubmit}
+        >
+          <div
+            className="  w-full
           bg-white
           mx-auto mt-4
           rounded-xl
         "
-      >
-        <div className="mb-10px flex justify-between gap-3 ">
-          <div className="mb-10px w-full">
-            <label className="form-label mb-24">Razón Social</label>
-            <div className="mt-5px">
-              <input
-                type="text"
-                placeholder="Razón Social"
-                value={formik.values.razonSocial}
-                onChange={formik.handleChange}
-                name="razonSocial"
-                id="razonSocial"
-                className={
-                  formik.errors.razonSocial
-                    ? "form-control mb-5px w-full border-2 is-invalid"
-                    : "form-control mb-5px w-full"
-                }
-              />
-            </div>
-          </div>
+          >
+            <div className="mb-10px flex justify-between gap-3 ">
+              <div className="mb-10px w-full">
+                <label className="form-label mb-24">Razón Social</label>
+                <div className="mt-5px">
+                  <input
+                    type="text"
+                    placeholder="Razón Social"
+                    value={formik.values.razonSocial}
+                    onChange={formik.handleChange}
+                    name="razonSocial"
+                    id="razonSocial"
+                    className={
+                      formik.errors.razonSocial
+                        ? "form-control mb-5px w-full border-2 is-invalid"
+                        : "form-control mb-5px w-full"
+                    }
+                  />
+                </div>
+              </div>
 
-          <div className="mb-10px w-full">
-            <label className="form-label mb-24">Email</label>
-            <div className="mt-5px">
-              <input
-                type="email"
-                className={
-                  formik.errors.email
-                    ? "form-control mb-5px w-full border-2 is-invalid"
-                    : "form-control mb-5px w-full"
-                }
-                placeholder="email"
-                id="email"
-                name="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-              />
-            </div>
-          </div>
-
-    
-        </div>
-
-        <div className="mb-10px flex justify-between gap-4">
-          <div className="mb-10px w-1/3">
-            <label className="form-label mb-24 ">Código Postal</label>
-            <div className="mt-5px">
-              <input
-                type="text"
-                className={
-                  formik.errors.codigoPostal
-                    ? "form-control mb-5px w-full border-2 is-invalid"
-                    : "form-control mb-5px w-full"
-                }
-                placeholder="Código Postal"
-                value={formik.values.codigoPostal}
-                onChange={formik.handleChange}
-                name="codigoPostal"
-                id="codigoPostal"
-              />
-            </div>
-          </div>
-
-          <div className="mb-10px w-1/3">
-            <label className="form-label mb-24">Régimen Fiscal</label>
-            <div className="mt-5px">
-              <select
-                value={formik.values.regimenFiscal}
-                // className="form-control form-select mb-5px w-full"
-                className={
-                  formik.errors.regimenFiscal
-                    ? "form-control form-select mb-5px w-full border-2 is-invalid"
-                    : "form-control form-select mb-5px w-full"
-                }
-                name="regimenFiscal"
-                id="regimenFiscal"
-                onChange={formik.handleChange}
-              >
-                <option value="">Selecciona una opción</option>
-                {catalogRegime.map((item: any) =>
-                  item.fisisca === "Sí" ? (
-                    <option key={item.clave} value={item.clave}>
-                      {item.descripcion}
-                    </option>
-                  ) : null
-                )}
-
-              </select>
+              <div className="mb-10px w-full">
+                <label className="form-label mb-24">Email</label>
+                <div className="mt-5px">
+                  <input
+                    type="email"
+                    className={
+                      formik.errors.email
+                        ? "form-control mb-5px w-full border-2 is-invalid"
+                        : "form-control mb-5px w-full"
+                    }
+                    placeholder="email"
+                    id="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                  />
+                </div>
+              </div>
             </div>
 
-          </div>
+            <div className="mb-10px flex justify-between gap-4">
+              <div className="mb-10px w-1/3">
+                <label className="form-label mb-24 ">Código Postal</label>
+                <div className="mt-5px">
+                  <input
+                    type="text"
+                    className={
+                      formik.errors.codigoPostal
+                        ? "form-control mb-5px w-full border-2 is-invalid"
+                        : "form-control mb-5px w-full"
+                    }
+                    placeholder="Código Postal"
+                    value={formik.values.codigoPostal}
+                    onChange={formik.handleChange}
+                    name="codigoPostal"
+                    id="codigoPostal"
+                  />
+                </div>
+              </div>
 
-          <div className="mb-10px w-1/3">
-            <label className="form-label mb-24">Uso de CFDI</label>
-            <div className="mt-5px">
-              <select
-                value={formik.values.usoCfdi}
-                className={
-                  formik.errors.usoCfdi
-                    ? "form-control form-select mb-5px w-full border-2 is-invalid"
-                    : "form-control form-select mb-5px w-full"
-                }
-                name="usoCfdi"
-                id="usoCfdi"
-                onChange={formik.handleChange}
-              >
-                <option value="">Selecciona una opción</option>
-              {catalogUseCFDI.map((item: any) => (
-                <option key={item.clave} value={item.clave}>
-                  {item.descripcion}
-                </option>
-              ))}
+              <div className="mb-10px w-1/3">
+                <label className="form-label mb-24">Régimen Fiscal</label>
+                <div className="mt-5px">
+                  <select
+                    value={formik.values.regimenFiscal}
+                    // className="form-control form-select mb-5px w-full"
+                    className={
+                      formik.errors.regimenFiscal
+                        ? "form-control form-select mb-5px w-full border-2 is-invalid"
+                        : "form-control form-select mb-5px w-full"
+                    }
+                    name="regimenFiscal"
+                    id="regimenFiscal"
+                    onChange={formik.handleChange}
+                  >
+                    <option value="">Selecciona una opción</option>
+                    {catalogRegime.map((item: any) =>
+                      item.fisisca === "Sí" ? (
+                        <option key={item.clave} value={item.clave}>
+                          {item.descripcion}
+                        </option>
+                      ) : null
+                    )}
+                  </select>
+                </div>
+              </div>
 
-              </select>
+              <div className="mb-10px w-1/3">
+                <label className="form-label mb-24">Uso de CFDI</label>
+                <div className="mt-5px">
+                  <select
+                    value={formik.values.usoCfdi}
+                    className={
+                      formik.errors.usoCfdi
+                        ? "form-control form-select mb-5px w-full border-2 is-invalid"
+                        : "form-control form-select mb-5px w-full"
+                    }
+                    name="usoCfdi"
+                    id="usoCfdi"
+                    onChange={formik.handleChange}
+                  >
+                    <option value="">Selecciona una opción</option>
+                    {catalogUseCFDI.map((item: any) => (
+                      <option key={item.clave} value={item.clave}>
+                        {item.descripcion}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
             </div>
 
-  
-          </div>
-        </div>
-
-        {/* <div className="flex justify-center mt-8">
+            {/* <div className="flex justify-center mt-8">
           <button
             disabled={!formik.isValid}
             type="submit"
@@ -176,14 +186,16 @@ export default function EditClient({ onClose, formik, isCreatingClient }: Props)
             Actualizar datos
           </button>
         </div> */}
-      </div>
+          </div>
 
-      <CommonAlert
-        message={message}
-        type={type}
-        onClose={() => setOpen(false)}
-        open={open}
-      />
-    </form>
+          <CommonAlert
+            message={message}
+            type={type}
+            onClose={() => setOpen(false)}
+            open={open}
+          />
+        </form>
+      )}
+    </>
   );
 }
